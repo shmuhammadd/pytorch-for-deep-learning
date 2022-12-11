78841b5bec95de2ef7c4a00acb4dcd8e267acdf5
shamsuddeen <shmuhammad.csc@buk.edu.ng>
2022-12-11 18:37:30 +0000

    chap2

---
+++ mymodule.py
@@ -0,0 +1,12 @@
+# function to add, subtract, multiply, divide two numbers
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
